f6d6ccbe3ed652a9cf379e0c0aa56760aa2a9621
Caio Carvalho <caio.chc93@gmail.com>
2021-04-06 04:06:39 -0300

    Base de estruturas e rotas
    Criada base de estruturação e rotas para servir de modelo para o desenvolvimento do projeto.

---
+++ src/pages/Agendamento/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function index() {
+  return (
+    <div>
+      Pagina com formulário de agendamento
+    </div>
+  );
+}
---
+++ src/routes.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Agendamento from './pages/Agendamento';
+import Consulta from './pages/Consulta';
+
+const routes = [{
+  path: '/',
+  component: Agendamento,
+  name: 'Agendamento',
+}, {
+  path: '/Consulta',
+  component: Consulta,
+  name: 'Consulta',
+}];
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      {routes.map(({ path, component }) => (
+        <Route exact key={path} path={path} component={component} />
+      ))}
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
